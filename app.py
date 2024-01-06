94c6a87506271298d2e19548a0b5422925d50277
yusuf <yusufbello@live.com>
2024-01-06 09:05:55 +0100

    added new file

---
+++ app.py
@@ -0,0 +1,40 @@
+#print('Jamal')
+
+# variables
+first_name = "Jamal"
+age = 10
+age2 = 10.0
+is_cool = True
+
+print(type(first_name))
+print(type(age))
+print(type(age2))
+print(type(is_cool))
+
+
+#concatenation
+# print('my name is Jamal and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+# print('my name is yusuf and i am 10 years old' )
+
+# print('my name is ')
+
+if name == ('jamal'):
+    print('welcome')
+
+
